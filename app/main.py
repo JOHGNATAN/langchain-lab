96e3a9aa0099b48291f97999552ee26e897b95ae
JOHGNATAN <johgnatanoperacional@gmail.com>
2025-09-07 16:04:00 -0300

    Adicionando Utils

--- app/main.py
+++ app/main.py
@@ -1,20 +1,4 @@
-from langchain_openai import OpenAI
-import json
-import os
-
-with open('app/secrets/secrets.json', 'r') as file:
-    secrets = json.load(file)
-
-os.environ["OPENAI_API_KEY"] = secrets["open_ai_key"]
-
-class LlmClient:
-    def __init__(self, modelo: str, criatividade: float):
-        self.client = OpenAI(model = modelo, temperature=criatividade)
-
-    def get_answer(self, prompt):
-        
-        for chunk in self.client.stream(prompt):
-            print(chunk, end='', flush=True)
+from app.utils import LlmClient
 
 
 if __name__ == "__main__":
