42a635dd6e4cec55895ea4697001ad2b9919a81d
JOHGNATAN <johgnatanoperacional@gmail.com>
2025-09-13 17:17:31 -0300

    update

--- app/main.py
+++ app/main.py
@@ -1,20 +1,9 @@
-from utils import LlmCompletionClient, LlmChatClient, load_secrets
+from utils import LlmChatClient, load_secrets
 
 
 if __name__ == "__main__":
     load_secrets()
     user_input = input("Enter your prompt: ")
 
-    while True:
-        model_type = input("Enter model type (completion/chat): ").strip().lower()
-        if model_type == "completion":
-            client = LlmCompletionClient("gpt-4o-mini", 0.3)
-            client.get_completion_answer(user_input)
-            break
-
-        elif model_type == "chat":
-            client = LlmChatClient("gpt-4o-mini", 0.3)
-            client.get_chat_answer(user_input)
-            break
-        else:
-            print("Invalid input. Please enter 'completion' or 'chat'.")
+    client = LlmChatClient("gpt-4o-mini", 0.3)
+    client.get_chat_answer(user_input)
