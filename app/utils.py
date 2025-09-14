e2705c64cae65b4589ab2bdb1d10a01320dfd7dd
JOHGNATAN <johgnatanoperacional@gmail.com>
2025-09-14 03:22:29 -0300

    update

--- app/utils.py
+++ app/utils.py
@@ -54,6 +54,7 @@ class LlmChatClient:
 
         with open(f"{self.destino}", "w", encoding='utf-8') as file:
             self.export_file = json.dump(self.history, file, ensure_ascii=False, indent=4)
+            
 
         
             
